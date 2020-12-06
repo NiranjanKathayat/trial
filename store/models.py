1808b9c3cd72fa4fa462aec53da9813c771c1774
Niranjan Kathayat <niraj2015niru@gmail.com>
2020-12-06 13:01:15 +0100

    changed in model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+#kkkkkkk
 
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
