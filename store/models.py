b8c8e38f945a7dbe95925e8757fb551d86ef642e
bhuwan <bkspoudel@gmail.com>
2020-12-06 14:08:11 +0100

    test1 changes

--- store/models.py
+++ store/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#kkkkkkk
-#jfldkjlfkdslkfslkjsdkjflksdjfklsdjlkfjsdlkjfklsjlkfjl
-# i am going to change some codes
-
-# something
-
+#test 1 conflict
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=55, null=True)
